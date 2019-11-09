b44f7d348fbb2139ace6e7aad9af93b99bd7dfcf
Gryyph <kevin.o.griffith@gmail.com>
2019-11-08 16:37:29 -0800

    callback

---
+++ main.js
@@ -0,0 +1,17 @@
+function titleize(arr, func){
+
+  const titled = arr.map( name => `Mx. ${name} fucking slut`);
+  func(titled);
+}
+
+function printCallback(arr) {
+  console.log(arr);
+  arr.forEach(name => console.log(name)); 
+  };
+
+
+titleize(["Mary", "Brian", "Leo"], printCallback);
+
+/*(names) => {
+  names.forEach(name => console.log(name));
+});*/
