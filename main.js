0a896469db45f0b4c23b779be3ee5e78b043a85f
Gryyph <kevin.o.griffith@gmail.com>
2019-11-08 20:46:27 -0800

    elephant

--- main.js
+++ main.js
@@ -15,3 +15,29 @@ titleize(["Mary", "Brian", "Leo"], printCallback);
 /*(names) => {
   names.forEach(name => console.log(name));
 });*/
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height= height;
+  this.tricks = tricks;
+
+  this.trumpet = function() {
+    console.log(`${this.name} goes phrRRRRRR`);
+  }
+
+  this.grow = function() {
+    this.height += 12;
+  }
+
+  this.addTrick = function(trick) {
+    this.tricks.push(trick);
+  }
+
+  this.play = function() {
+    var i = Math.floor(Math.random() * Math.floor(this.heigt.length));
+    console.log(`${this.name} is ${this.tricks[i]}`);
+  }
+
+}
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
