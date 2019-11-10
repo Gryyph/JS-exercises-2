1d57513e8ace57d6eb22951568df555022a19ed4
Gryyph <kevin.o.griffith@gmail.com>
2019-11-10 14:42:35 -0800

    finish elephant

--- main.js
+++ main.js
@@ -20,24 +20,25 @@ function Elephant(name, height, tricks) {
   this.name = name;
   this.height= height;
   this.tricks = tricks;
-
-  this.trumpet = function() {
-    console.log(`${this.name} goes phrRRRRRR`);
-  }
-
-  this.grow = function() {
-    this.height += 12;
-  }
-
-  this.addTrick = function(trick) {
-    this.tricks.push(trick);
-  }
-
-  this.play = function() {
-    var i = Math.floor(Math.random() * Math.floor(this.heigt.length));
-    console.log(`${this.name} is ${this.tricks[i]}`);
-  }
-
 }
+
+Elephant.prototype.trumpet = function() {
+  console.log(`${this.name} goes phrRRRRRR`);
+};
+
+Elephant.prototype.grow = function() {
+  this.height += 12;
+};
+
+Elephant.prototype.addTrick = function(trick) {
+  this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function() {
+    var i = Math.floor(Math.random() * Math.floor(this.height.length));
+    console.log(`${this.name} is ${this.tricks[i]}`);
+  };
+
+
 
 let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
